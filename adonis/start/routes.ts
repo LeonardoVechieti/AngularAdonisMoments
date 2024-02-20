59fc9364bca206905263b58fa643605320f1f35a
Leonardo Vechieti <leonardo.vechieti@tc2code.com.br>
2024-02-20 10:06:54 -0300

    add: api em adonis

---
+++ adonis/start/routes.ts
@@ -0,0 +1,10 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.get('/', async () => {
+    return { hello: 'world' }
+  })
+
+  Route.resource('/moments', 'MomentsController').apiOnly()
+  Route.post('/moments/:momentId/comments', 'CommentsController.store')
+}).prefix('/api')
